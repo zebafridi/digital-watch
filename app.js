ab3da13e4e76f6ee8f566ad4911f1ba4b3b78718
aurang zeb <zebafridi71@gmail.com>
2024-01-23 08:01:53 +0500

    first commit

---
+++ app.js
@@ -0,0 +1,13 @@
+let freshDate= new Date();
+
+ let hour=freshDate.getHours();
+
+
+ let minut=freshDate.getMinutes();
+ let second=freshDate.getSeconds();
+
+
+ document.getElementById('hour').innerText=hour;
+
+ document.getElementById('minuts').innerText=minut;
+ document.getElementById('seconds').innerText=second;
